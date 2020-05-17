88f959f29c8b27b7da750a91e718f7ec316491fe
lavinamulani <indianyouth98@gmail.com>
2020-05-17 13:24:38 -0700

    first commit

---
+++ src/ResumeBuilder/PDFTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ResumeBuilder;
+
+import com.itextpdf.text.Document;
+import java.io.IOException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+/**
+ *
+ * @author peace
+ */
+public class PDFTest {
+    public void createPdf(String name) throws IOException {
+        try (PDDocument document = new PDDocument()) {
+            PDPage blankPage = new PDPage();
+            document.addPage( blankPage );
+            document.save(name);
+        } 
+        Document doc = new Document();
+    }
+    public static void main(String args[]) throws IOException {
+
+    }
+    
+}
